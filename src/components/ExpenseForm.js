b3860d14866ed3f2fb675e45bd8fb07c49f49845
geomarb <gtcbrs@gmail.com>
2019-06-24 17:39:31 +0100

    Style project

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -61,12 +61,14 @@ export default class ExpenseForm extends React.Component {
                         type="text"
                         placeholder="Description"
                         autoFocus
+                        className="text-input"
                         value={this.state.description}
                         onChange={this.onDescriptionChange}
                     />
                     <input
                         type="text"
                         placeholder="Amount"
+                        className="text-input"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
                     />
@@ -80,6 +82,7 @@ export default class ExpenseForm extends React.Component {
                     />
                     <textarea
                         placeholder="Add a note for your expense (optional)"
+                        className="textarea"
                         value={this.state.note}
                         onChange={this.onNoteChange}
                     >
